aa60753519b347940974703bf70beb5da1119a44
bajpaia <32839326+bajpaia@users.noreply.github.com>
2017-10-30 14:59:43 +0100

    changes

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,6 +2,5 @@ def process_user_query(query_string):
     result=[]
     new_list= query_string.split()
     for i in new_list:
-        result.append('Hi! ' + i)
-
+        result.append('Hola! ' + i +' como estas?')
     return result
