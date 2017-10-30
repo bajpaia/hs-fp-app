5857b424d79c71ff384db4e5689f21be0856f07d
bajpaia <32839326+bajpaia@users.noreply.github.com>
2017-10-30 15:54:33 +0100

    changes

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,7 @@
 def process_user_query(query_string):
     result=[]
-    new_list= query_string.split()
-    for i in new_list:
-        result.append('Hola! ' + i +' como estas?')
+    word_list= query_string.split()
+    for word in word_list:
+        if word[0].isupper()==True:
+            result.append('Hola! ' + word +' como estas?')
     return result
